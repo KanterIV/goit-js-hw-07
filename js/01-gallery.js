6c6b5d8500d9bda469bf0e86690ae156b7ba9d30
KanterIV <Kanterigor@gmail.com>
2023-07-20 14:01:50 +0200

    task #1 is ready

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryItemEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+  )
+  .join("");
+galleryEl.innerHTML = galleryItemEl;
+
+galleryEl.addEventListener("click", onGalleryModalOpenClick);
+
+function onGalleryModalOpenClick(event) {
+  event.preventDefault();
+  if (event.target.className !== "gallery__image") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
